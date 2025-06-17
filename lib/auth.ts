7c3c914f6faac69457d511ed7ff4b8733fa23fdc
farjanaIslamNipa <nipa.farzana87@gmail.com>
2025-06-17 19:28:43 +0600

    working on next auth

---
+++ lib/auth.ts
@@ -0,0 +1,25 @@
+import { NextAuthOptions } from "next-auth";
+import  CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials){
+        if(!credentials?.email || !credentials?.password){
+          throw new Error("Email or password is missing")
+        }
+
+        try {
+          await 
+        } catch (error) {
+          
+        }
+      }
+    })
+  ]
+}
