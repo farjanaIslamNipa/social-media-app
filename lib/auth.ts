c0cd2c64452b0fb576429f019eca8815058a728c
farjanaIslamNipa <nipa.farzana87@gmail.com>
2025-06-24 18:58:42 +0600

    Create auth option

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,9 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextAuthOptions } from "next-auth";
 import  CredentialsProvider from "next-auth/providers/credentials";
+import { connectToDatabase } from "./db";
+import User from "@/models/Users";
+import bcrypt from "bcryptjs";
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -15,11 +19,47 @@ export const authOptions: NextAuthOptions = {
         }
 
         try {
-          await 
-        } catch (error) {
-          
+          await connectToDatabase()
+          const user = await User.findOne({email: credentials.email})
+
+          if(!user){
+            throw new Error("No user found")
+          }
+
+          const isValid = await bcrypt.compare(
+            credentials.password,
+            user.password
+          )
+
+          if(!isValid){
+            throw new Error("Invalid password")
+          }
+
+          return {
+            id: user._id.toString(),
+            email: user.email
+          }
+        } catch (error : any) {
+          throw new Error(error.message || 'Something went wrong')
         }
       }
     })
-  ]
+  ],
+  callbacks: {
+    async jwt({token, user}){
+      if(user){
+        token.id = user.id
+      }
+      return token
+    },
+    async session({session, token}){
+      if(session.user){
+        session.user.id = token.id as string
+      }
+      return session
+    }
+  },
+  session:{
+    strategy: "jwt"
+  }
 }
